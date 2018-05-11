c71bf7c06fb9fc767b93dbe4488379a96d399da2
uranpro <uran.pro@gmail.com>
2018-05-11 05:17:47 +0300

    + added DropDown cart

--- DropDownTestProject/Controller/Picker/DropDownViewController.swift
+++ DropDownTestProject/Controller/Picker/DropDownViewController.swift
@@ -7,8 +7,33 @@
 //
 
 import UIKit
-//import DropDown
+import DropDown
 
 class DropDownViewController: UIViewController {
+    
+    @IBOutlet weak var dropDownButton: UIButton!
+    let dropDown = DropDown()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        dropDownSetup()
+    }
+    
+    // MARK: Helerps
+    
+    func dropDownSetup() {
+        DropDown.startListeningToKeyboard()
+        DropDown.setupDefaultAppearance()
+        
+        dropDown.anchorView = dropDownButton
+        dropDown.dataSource = ["Car", "Motorcycle", "Truck"]
+    }
+    
+    // MARK: User interactions
+    
+    @IBAction func buttonTap(_ sender: Any) {
+        dropDown.show()
+    }
     
 }
